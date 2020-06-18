84968de89dd645ef549e71eb4b27f795e04bb7d9
BarbyFrancia <barbyfrancia@gmail.com>
2020-06-18 10:57:22 -0400

    app integrada con algoritmo, maskify y css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-import validator from './validator.js';
+//Guardar rut escrito en variable enviar, para llevarlo al validador
 
-console.log(validator);
+var enviar = document.getElementById('enviar');
+enviar.addEventListener("click", siguiente);
+
+
+function siguiente() {
+    var rut = document.getElementById('rut').value;
+    rut = rut.split('.').join('');
+    rut = rut.split('-').join('');
+
+    console.log(rut.length);
+ /*    rut = parseInt(rut);
+    console.log(rut); */
+    
+    if (rut.length < 8) {
+        console.log('ERROR');
+
+    } else {
+        console.log('SUCCESS');
+
+        
+        document.getElementById('validador').classList.remove('hide');
+        document.getElementById('principal').classList.add('hide');
+
+        var validartarjeta = document.getElementById('validartarjeta');
+        validartarjeta.addEventListener("click", validar); 
+    }
+}
