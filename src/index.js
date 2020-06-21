b853eef0c423fdea5516268316e9ca51a7c1c12f
Maritzu Zúñiga <maritzudelosangeles@gmail.com>
2020-06-20 22:39:07 -0400

    refactorizaciÃn de funciones js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,60 @@
-//Guardar rut escrito en variable enviar, para llevarlo al validador
-
 var enviar = document.getElementById('enviar');
 enviar.addEventListener("click", siguiente);
+// SCOPE
+// HOISTING
+var validartarjeta = document.getElementById('validartarjeta');
+validartarjeta.addEventListener("click", validar);
 
+var volver = document.getElementById('volver');
+volver.addEventListener("click", reinciar);
+
+var volverin = document.getElementById('volverin');
+volverin.addEventListener("click", reinciar);
 
 function siguiente() {
-    var rut = document.getElementById('rut').value;
-    rut = rut.split('.').join('');
-    rut = rut.split('-').join('');
 
-    //console.log(rut.length);
- /*    rut = parseInt(rut);
-    console.log(rut); */
-    
-    if (rut.length < 8) {
-        console.log('ERROR');
+  var rut = document.getElementById('rut').value;
+  rut = rut.split('.').join('');
+  rut = rut.split('-').join('');
 
-    } else {
-        console.log('SUCCESS');
+  console.log(rut.length);
+  /*    rut = parseInt(rut);
+     console.log(rut); */
 
-        
-        document.getElementById('validador').classList.remove('hide');
-        document.getElementById('principal').classList.add('hide');
+  if (rut.length < 8) {
+    console.log('ERROR');
 
-        var validartarjeta = document.getElementById('validartarjeta');
-        validartarjeta.addEventListener("click", validar); 
-    }
+  } else {
+    console.log('SUCCESS');
+
+    document.getElementById('validador').classList.remove('hide');
+    document.getElementById('principal').classList.add('hide');
+  }
 }
+
+function validar() {
+  var cardNumber = document.getElementById('card').value;
+  var total = validarTarjeta(cardNumber);
+
+  if (total != 0) {
+    alert('Tarjeta Inválida');
+    maskify(cardNumber);
+    document.getElementById('validador').classList.add('hide');
+    document.getElementById('invalida').classList.remove('hide');
+
+  } else {
+    alert('Tarjeta Válida');
+    maskify(cardNumber);
+    document.getElementById('validador').classList.add('hide');
+    document.getElementById('aprobado').classList.remove('hide');
+  }
+}
+
+function reinciar() {
+  console.log("reinciar");
+  document.getElementById('principal').classList.remove('hide');
+  document.getElementById('validador').classList.add('hide');
+  document.getElementById('aprobado').classList.add('hide');
+  document.getElementById('invalida').classList.add('hide');
+}
+//4137894711755904
--- src/validator.js
+++ src/validator.js
@@ -1,100 +1,81 @@
 function maskify(cc) {
-  visibles = cc.slice(-4),
+    visibles = cc.slice(-4),
       enmascarado = '';
-
-  for (var i = (cc.length) - 4; i > 0; i--) {
+  
+    for (var i = (cc.length) - 4; i > 0; i--) {
       enmascarado += '#';
+    }
+    alert(enmascarado + visibles);
   }
-  //alert(enmascarado + visibles);
-  return enmascarado + visibles;
-}
-
-function validar() {
-
-  var cardNumber = document.getElementById('card').value;
-
-  //console.log(cardNumber);
-
-  // JUNTAR NUMEROS
-  cardNumber = cardNumber.split(' ').join("");
-
-  // VALIDADOR SI ES TARJETA
-
-  if (parseInt(cardNumber) <= 0 || (!/\d{15,16}(~\W[a-zA-Z])*$/.test(cardNumber)) || cardNumber.length > 16) {
-
+  
+  function validarTarjeta(cardNumber) {
+    console.log(cardNumber);
+  
+    // JUNTAR NUMEROS
+    cardNumber = cardNumber.split(' ').join("");
+  
+    // VALIDADOR SI ES TARJETA
+    if (parseInt(cardNumber) <= 0 || (!/\d{15,16}(~\W[a-zA-Z])*$/.test(cardNumber)) || cardNumber.length > 16) {
+  
       alert('Debe ingresar 16 numeros de su tarjeta de crédito');
-
-  } else {
-
+  
+    } else {
+  
       // CREAR ARREGLO, METER NUMEROS E INVERTIR
-
+  
       var carray = [];
       var largoNumeros = cardNumber.length;
       var validador = 0;
-
+  
       for (i = 0; i < largoNumeros; i++) {
-          carray[i] = cardNumber.charAt(i);
+        carray[i] = cardNumber.charAt(i);
       }
-
-      //console.log(carray);
+  
+      console.log(carray);
       carray = carray.reverse();
-      //console.log(carray);
-
+      console.log(carray);
+  
       //SELECCIONAR NUMEROS EN POSICION PAR 
-
+  
       for (i = 0; i < carray.length; i++) {
-
-          var number = carray[i];
-
-          if (i % 2 != 0) {
-              //console.log('impar' + number);
-
-              //MULTIPLICAR PARES POR DOS
-              number = number * 2;
-
-              if (number >= 10) {
-
-                  //SI ES MAYOR O IGUAL A 10 SUMAR DIGITOS DEL RESULTADO
-
-                  var number = number.toString().split('').map(Number).reduce(function (a, b) {
-                      return a + b;
-                  }, 0);
-
-              }
-
-          } else {
-              //console.log('par' + number);
+  
+        var number = carray[i];
+  
+        if (i % 2 != 0) {
+          console.log('impar' + number);
+  
+          //MULTIPLICAR PARES POR DOS
+          number = number * 2;
+  
+          if (number >= 10) {
+  
+            //SI ES MAYOR O IGUAL A 10 SUMAR DIGITOS DEL RESULTADO
+  
+            var number = number.toString().split('').map(Number).reduce(function (a, b) {
+              return a + b;
+            }, 0);
+  
           }
-
-          //SUMAR TODOS LOS DIGITOS Y VER SI SON MULTIPLOS DE 10
-
-          number = parseInt(number);
-          //console.log(i + 'NUMEROS' + number);
-
-          validador = validador + number;
-
-          //console.log('SUMA ' + validador);
+  
+        } else {
+          console.log('par' + number);
+        }
+  
+        //SUMAR TODOS LOS DIGITOS Y VER SI SON MULTIPLOS DE 10
+  
+        number = parseInt(number);
+        console.log(i + 'NUMEROS' + number);
+  
+        validador = validador + number;
+  
+        console.log('SUMA ' + validador);
       }
-
+  
       var total = validador % 10;
-      //console.log ('total ' + total);
-
-      if(total != 0){
-          alert('Tarjeta Inválida');
-          maskify(cardNumber);
-          document.getElementById('validador').classList.add('hide');
-          document.getElementById('invalida').classList.remove('hide');
-          document.getElementById("invalido").innerHTML = (maskify(cardNumber));
-      }else {
-          alert('Tarjeta Válida');
-          maskify(cardNumber);
-          document.getElementById('validador').classList.add('hide');
-          document.getElementById('aprobado').classList.remove('hide');
-          document.getElementById("valido").innerHTML = (maskify(cardNumber));
-      }
-
+      console.log('total ' + total);
+      
+      return total;
+    }
   }
-}
-
 
 
